7f9e89163b8696f2e48bf60018b80ab0c174d5a8
Diego Macrini <macrini@proximify.ca>
2025-02-05 17:05:48 -0500

    fix

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,98 +2,110 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const DotenvWebpack = require("dotenv-webpack");
-const { ModuleFederationPlugin } = require("webpack").container;
+const webpack = require("webpack");
+const { ModuleFederationPlugin } = webpack.container;
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
-    publicPath: "/",
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react"],
-          },
-        },
-      },
-      {
-        test: /\.(woff2?|ttf|eot)$/,
-        type: "asset/resource",
-        generator: {
-          filename: "fonts/[name].[hash][ext]",
-        },
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg|ico)$/,
-        type: "asset",
-        parser: {
-          dataUrlCondition: {
-            maxSize: 8 * 1024, // 8kb
-          },
-        },
-        generator: {
-          filename: "images/[name].[hash][ext]",
-        },
-      },
-    ],
-  },
-  plugins: [
-    new DotenvWebpack(),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-    new CopyPlugin({
-      patterns: [
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].[contenthash].js",
+      publicPath: "/",
+      clean: true,
+    },
+    module: {
+      rules: [
         {
-          from: "public",
-          to: ".",
-          globOptions: {
-            ignore: ["**/index.html"],
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-react"],
+            },
+          },
+        },
+        {
+          test: /\.(woff2?|ttf|eot)$/,
+          type: "asset/resource",
+          generator: {
+            filename: "fonts/[name].[hash][ext]",
+          },
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg|ico)$/,
+          type: "asset",
+          parser: {
+            dataUrlCondition: {
+              maxSize: 8 * 1024, // 8kb
+            },
+          },
+          generator: {
+            filename: "images/[name].[hash][ext]",
           },
         },
       ],
-    }),
-    new ModuleFederationPlugin({
-      name: "website",
-      remotes: {
-        components: `components@${process.env.COMPONENTS_MODULE_URL}/remoteEntry.js`,
-      },
-      shared: {
-        react: { singleton: true },
-        "react-dom": { singleton: true },
-      },
-    }),
-  ],
-  devServer: {
-    historyApiFallback: true,
-    static: {
-      directory: path.join(__dirname, "dist"),
     },
-    port: 3000,
-    proxy: {
-      "/content.json": "http://localhost:3000",
-      "/assets": "http://localhost:3000",
+    plugins: [
+      new DotenvWebpack({
+        systemvars: true, // Load all system variables as well
+      }),
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(
+          isProduction ? "production" : "development"
+        ),
+      }),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "public",
+            to: ".",
+            globOptions: {
+              ignore: ["**/index.html"],
+            },
+          },
+        ],
+      }),
+      new ModuleFederationPlugin({
+        name: "website",
+        remotes: {
+          components: `components@${process.env.COMPONENTS_MODULE_URL}/remoteEntry.js`,
+        },
+        shared: {
+          react: { singleton: true },
+          "react-dom": { singleton: true },
+        },
+      }),
+    ],
+    devServer: {
+      historyApiFallback: true,
+      static: {
+        directory: path.join(__dirname, "dist"),
+      },
+      port: 3000,
+      proxy: {
+        "/content.json": "http://localhost:3000",
+        "/assets": "http://localhost:3000",
+      },
     },
-  },
-  optimization: {
-    moduleIds: "deterministic",
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
+    optimization: {
+      moduleIds: "deterministic",
+      runtimeChunk: "single",
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendors",
+            chunks: "all",
+          },
         },
       },
     },
-  },
+  };
 };
