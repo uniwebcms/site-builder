c35b36fe8b0b44b692f665f3c7b3abb9d5f2b14c
Diego Macrini <macrini@proximify.ca>
2025-02-07 17:36:25 -0500

    simpler approach

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,29 +1,4 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, useParams, useLocation, useNavigate } from 'react-router-dom';
-import { ChildBlocks } from './components/PageRenderer.js';
-import Link from './components/Link.js';
-import SafeHtml from './components/SafeHtml.js';
-import App from './App.js';
 import configData from '../public/example.json';
 import { initRTE } from 'uniweb-re';
 
-const modulePromise = initRTE(import('WebsiteRemote/widgets'), configData);
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-uniweb.childBlockRenderer = ChildBlocks;
-uniweb.routingComponents = {
-    Link,
-    SafeHtml,
-    useNavigate,
-    useParams,
-    useLocation,
-};
-
-root.render(
-    <BrowserRouter>
-        <App modulePromise={modulePromise} />
-    </BrowserRouter>
-);
+initRTE(import('WebsiteRemote/widgets'), configData);
