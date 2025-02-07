9407de641208a84e18942ea413f560f15a2c641e
Diego Macrini <macrini@proximify.ca>
2025-02-07 14:18:30 -0500

    Simpler code

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,14 +5,10 @@ import { ChildBlocks } from './components/PageRenderer.js';
 import Link from './components/Link.js';
 import SafeHtml from './components/SafeHtml.js';
 import App from './App.js';
+import configData from '../public/example.json';
+import { initRTE } from 'uniweb-re';
 
-const modulePromise = import('WebsiteRemote/widgets')
-    .then((module) => {
-        // Handle double default wrapping issue when CommonJS is imported into ES6 module
-        uniweb.setRemoteComponents(module?.default?.default || module?.default);
-        return true;
-    })
-    .catch((error) => console.error('Failed to load module:', error));
+const modulePromise = initRTE(import('WebsiteRemote/widgets'), configData);
 
 const container = document.getElementById('root');
 const root = createRoot(container);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-// import('./bootstrap.js');
-import configData from '../public/example.json';
-import { Uniweb } from 'uniweb-re';
-
-window.uniweb = new Uniweb(configData);
-
 import('./bootstrap.js');
